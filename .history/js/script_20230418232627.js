3c71593780f9a4d217bb97e82d0d54982969bb4e
kayacancode <62274079+kayacancode@users.noreply.github.com>
2023-04-18 23:36:55 -0500

    adding map

--- .history/js/script_20230418232627.js
+++ .history/js/script_20230418232627.js
@@ -1,7 +1,7 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 10,
-      center: { lat: 35.117500, lng: -89.9711078 },
+      center: { lat: 41.88199940173343, lng: -87.62676460031378 },
     });
   }
   
--- .history/js/script_20230418233644.js
+++ .history/js/script_20230418233644.js
@@ -1,7 +1,7 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 10,
-      center: { lat: 35.117500, lng: -89.9711078 },
+      center: { lat: 35.117500, lng: --89.9711078 },
     });
   }
   
